2eea0e6ac9a58ec258a0f1b0d25f3e92775dcf43
ZohaibFais <murtuza.tcc@gmail.com>
2024-02-12 19:31:35 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Q1.
+
+// var firstName = prompt("Enter first name: ")
+// var lastName = prompt("Enter last name: ")
+
+// var fullName = firstName + " " + lastName
+
+// alert("Hi " + fullName)
+
+//Q2.
+
+// var mobile = prompt("Enter your favorite mobile phone: ")
+// var length = mobile.length
+
+// document.write("My favorite phone is: " + mobile)
+// document.write("<br>")
+// document.write("Length of string: " + length)
+
+//Q3.
+
+// var country = "Pakistani"
+// document.write("String: " + country)
+// document.write("<br>")
+// var searchLetter = "n"
+// var index = country.indexOf("n")
+// document.write("Index of 'n': " + inde)
